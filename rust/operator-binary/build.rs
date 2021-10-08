b0ae175dd5ed871e063eff439df0d79f40afdd09
maltesander <malte.sander.it@gmail.com>
2021-10-08 16:22:37 +0200

    Fixed fmt.

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,6 +1,6 @@
-use stackable_operator::crd::CustomResourceExt;
 use stackable_hive_crd::commands::{Restart, Start, Stop};
-use stackable_hive_crd::{HiveCluster, DatabaseConnection};
+use stackable_hive_crd::{DatabaseConnection, HiveCluster};
+use stackable_operator::crd::CustomResourceExt;
 
 fn main() -> Result<(), stackable_operator::error::Error> {
     built::write_built_file().expect("Failed to acquire build-time information");
