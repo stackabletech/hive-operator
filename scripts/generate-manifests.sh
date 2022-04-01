b6bbeba73fb829a027e610a81ace8a412f532344
Stacky McStackface <github@stackable.de>
2022-04-01 21:12:39 +0000

    Update templated files to rev 11fe2e0 (#161)
    Automatically created PR based on commit 11fe2e042b8e7106d7f7a3bdf07a09d9be7ab0ed in stackabletech/operator-templating repo.
    
    Triggered by:
    Manual run triggered by: soenkeliebau with message [Roll out a number of changes: test templating, fixing of manifest generation, disabling of detect secret linter]

--- scripts/generate-manifests.sh
+++ scripts/generate-manifests.sh
@@ -10,12 +10,11 @@ helm template --output-dir "$tmp" \
               --name-template hive-operator \
               deploy/helm/hive-operator
 
-while IFS= read -r -d '' file
-do
-    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' /dev/stdin < "$file"
-    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' /dev/stdin < "$file"
-    sed -i '/# Source: .*/d' "$file"
-done <   <(find "$tmp" -type f)
+for file in "$tmp"/hive-operator/*/*; do
+  yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' /dev/stdin < "$file"
+  yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' /dev/stdin < "$file"
+  sed -i '/# Source: .*/d' "$file"
+done
 
 cp -r "$tmp"/hive-operator/*/* deploy/manifests/
 
