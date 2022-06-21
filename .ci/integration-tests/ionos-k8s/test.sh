ff765fb6475125de69266264081e5ccf7e351dd3
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-21 15:37:51 +0200

    removed CI files

--- .ci/integration-tests/ionos-k8s/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/hive-operator.git
-(cd hive-operator/ && ./scripts/run_tests.sh --parallel 1)
-exit_code=$?
-./operator-logs.sh hive > /target/hive-operator.log
-exit $exit_code
