96f177d0edede064fd1a4b1b940fee9eaa0eea27
Malte Sander <contact@maltesander.com>
2023-01-10 11:40:45 +0000

    Removed deploy crd folder (#289)
    # Description
    
    fixes https://github.com/stackabletech/hive-operator/issues/147
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,3 @@
-use stackable_hive_crd::HiveCluster;
-use stackable_operator::crd::CustomResourceExt;
-
-fn main() -> Result<(), stackable_operator::error::Error> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    HiveCluster::write_yaml_schema("../../deploy/crd/hivecluster.crd.yaml")?;
-
-    Ok(())
 }
