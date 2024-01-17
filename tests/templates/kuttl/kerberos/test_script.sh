5d9ddb484589ec3ede3166bf17725d7403415dcc
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-01-17 13:59:18 +0100

    minor change to script

--- tests/templates/kuttl/kerberos/test_script.sh
+++ tests/templates/kuttl/kerberos/test_script.sh
@@ -5,9 +5,6 @@ klist -k /stackable/kerberos/keytab
 kinit -kt /stackable/kerberos/keytab hive/access-hive.$NAMESPACE.svc.cluster.local
 klist
 
-cat /stackable/conf/hdfs_mount/core-site.xml
-cat /stackable/conf/hdfs_mount/hdfs-site.xml
-
 export KERBEROS_REALM=$(grep -oP 'default_realm = \K.*' /stackable/kerberos/krb5.conf)
 cat /stackable/conf/hdfs_mount/core-site.xml | sed -e 's/${env.KERBEROS_REALM}/'"$KERBEROS_REALM/g" > /stackable/conf/hive/core-site.xml
 cat /stackable/conf/hdfs_mount/hdfs-site.xml | sed -e 's/${env.KERBEROS_REALM}/'"$KERBEROS_REALM/g" > /stackable/conf/hive/hdfs-site.xml
