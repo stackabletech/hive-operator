cf08e5737653ead64442db4ba0059597cc4e87bc
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-01-23 12:20:10 +0100

    Update rust/crd/src/security.rs
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- rust/crd/src/security.rs
+++ rust/crd/src/security.rs
@@ -4,10 +4,6 @@ use stackable_operator::schemars::{self, JsonSchema};
 #[derive(Clone, Debug, Deserialize, Eq, Hash, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AuthenticationConfig {
-    /// Name of the SecretClass providing the tls certificates for the WebUIs.
-    #[serde(default = "default_tls_secret_class")]
-    pub tls_secret_class: String,
-
     /// Kerberos configuration.
     pub kerberos: KerberosConfig,
 }
