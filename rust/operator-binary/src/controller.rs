c0f4b580883eb6d3cb6d9cd82f114637cc29b2ee
Maxi Wittich <maximilian.wittich@stackable.tech>
2025-07-02 18:59:38 +0200

    Updating function name everywhere

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -804,7 +804,7 @@ fn build_rolegroup_services(
 /// The rolegroup [`StatefulSet`] runs the rolegroup, as configured by the administrator.
 ///
 /// The [`Pod`](`stackable_operator::k8s_openapi::api::core::v1::Pod`)s are accessible through the
-/// corresponding [`Service`] (from [`build_rolegroup_service`]).
+/// corresponding [`Service`] (from [`build_rolegroup_services`]).
 #[allow(clippy::too_many_arguments)]
 fn build_metastore_rolegroup_statefulset(
     hive: &v1alpha1::HiveCluster,
