c23a713978b5e86f543b45017a311c0921922de1
Techassi <git@techassi.dev>
2025-10-29 11:38:28 +0100

    feat: Re-add EoS checker
    This re-adds commit a49e4a702261b36a41e9ea36816a01a22671f4c1 (modified).
    This reverts commit 32c83a9a623b88b3a633c8444be4918a0add7dae.
    
    Co-authored-by: Xenia Fischer <xenia.fischer@stackable.tech>

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -16,10 +16,11 @@ mod service;
 use std::sync::Arc;
 
 use clap::Parser;
-use futures::stream::StreamExt;
+use futures::{FutureExt, StreamExt};
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, RunArguments},
+    eos::EndOfSupportChecker,
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Service},
@@ -63,18 +64,19 @@ async fn main() -> anyhow::Result<()> {
     match opts.cmd {
         Command::Crd => HiveCluster::merged_crd(HiveClusterVersion::V1Alpha1)?
             .print_yaml_schema(built_info::PKG_VERSION, SerializeOptions::default())?,
-        Command::Run(ProductOperatorRun {
+        Command::Run(RunArguments {
             product_config,
             watch_namespace,
             operator_environment: _,
-            telemetry,
-            cluster_info,
+            maintenance,
+            common,
         }) => {
             // NOTE (@NickLarsenNZ): Before stackable-telemetry was used:
             // - The console log level was set by `HIVE_OPERATOR_LOG`, and is now `CONSOLE_LOG` (when using Tracing::pre_configured).
             // - The file log level was set by `HIVE_OPERATOR_LOG`, and is now set via `FILE_LOG` (when using Tracing::pre_configured).
             // - The file log directory was set by `HIVE_OPERATOR_LOG_DIRECTORY`, and is now set by `ROLLING_LOGS_DIR` (or via `--rolling-logs <DIRECTORY>`).
-            let _tracing_guard = Tracing::pre_configured(built_info::PKG_NAME, telemetry).init()?;
+            let _tracing_guard =
+                Tracing::pre_configured(built_info::PKG_NAME, common.telemetry).init()?;
 
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
@@ -86,6 +88,11 @@ async fn main() -> anyhow::Result<()> {
                 description = built_info::PKG_DESCRIPTION
             );
 
+            let eos_checker =
+                EndOfSupportChecker::new(built_info::BUILT_TIME_UTC, maintenance.end_of_support)?
+                    .run()
+                    .map(anyhow::Ok);
+
             let product_config = product_config.load(&[
                 "deploy/config-spec/properties.yaml",
                 "/etc/stackable/hive-operator/config-spec/properties.yaml",
@@ -93,7 +100,7 @@ async fn main() -> anyhow::Result<()> {
 
             let client = stackable_operator::client::initialize_operator(
                 Some(OPERATOR_NAME.to_string()),
-                &cluster_info,
+                &common.cluster_info,
             )
             .await?;
             let event_recorder = Arc::new(Recorder::new(
@@ -109,7 +116,7 @@ async fn main() -> anyhow::Result<()> {
                 watcher::Config::default(),
             );
             let config_map_store = hive_controller.store();
-            hive_controller
+            let hive_controller = hive_controller
                 .owns(
                     watch_namespace.get_api::<Service>(&client),
                     watcher::Config::default(),
@@ -159,7 +166,9 @@ async fn main() -> anyhow::Result<()> {
                         }
                     },
                 )
-                .await;
+                .map(anyhow::Ok);
+
+            futures::try_join!(hive_controller, eos_checker)?;
         }
     }
 
